a9cabce81f4308e031df428020deb5d9efd5b2fc
SabrinaTavera <sabrina.tavera@hotmail.com>
2023-03-15 10:29:59 -0300

    fundamentos-node [em andamento] Tipos de rotas - GET / POST / PUT / PATCH / DELETE

--- chapter_01/src/index.js
+++ chapter_01/src/index.js
@@ -6,6 +6,35 @@ const express =  require('express')
 //constante que faz a chamada do serviço do express 
 const app = express();
 
+/**
+ * GET      => Buscar informação 
+ * POST     => Inserir uma informação no servidor 
+ * PUT      => Alterar uma informação no servidor 
+ * PATCH    => Alterar uma informação específica 
+ * DELETE   => Deletar uma informação no servidor 
+ */
+
+app.get("/courses", (request, response) =>{
+    return response.json(["Curso 1", "Curso 2", "Curso 3"])
+})
+
+app.post("/courses", (request, response) =>{
+    return response.json(["Curso 1", "Curso 2", "Curso 3"])
+})
+
+app.put("/courses/:id", (request, response) =>{
+    return response.json(["Curso 1", "Curso 2", "Curso 3"])
+})
+
+app.patch("/courses/:id", (request, response) =>{
+    return response.json(["Curso 1", "Curso 2", "Curso 3"])
+})
+
+app.delete("/courses/:id", (request, response) =>{
+    return response.json(["Curso 1", "Curso 2", "Curso 3"])
+})
+
+
 app.get("/", (request, response) =>{
 
     //Método sendo deixa enviar uma msg na requisição
